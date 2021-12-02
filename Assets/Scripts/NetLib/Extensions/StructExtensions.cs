0d620429db5b0e378b2cac77614f98cb5f99c8cb
neo-level <frederik@neo-level.com>
2021-12-02 21:17:24 +0100

    Created extension methods to serialize to and from a struct

--- Assets/Scripts/NetLib/Extensions/StructExtensions.cs
+++ Assets/Scripts/NetLib/Extensions/StructExtensions.cs
@@ -1,18 +1,35 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+using System.Runtime.InteropServices;
 
-public class StructExtensions : MonoBehaviour
+namespace NetLib.Extensions
 {
-    // Start is called before the first frame update
-    void Start()
+    public static class StructExtensions
     {
-        
-    }
+        public static T ToStruct<T>(this byte[] data) where T : struct
+        {
+            var size = Marshal.SizeOf(typeof(T));
+            var pointer = Marshal.AllocHGlobal(size);
+            int startIndex = 0;
+            Marshal.Copy(data, startIndex, pointer, size);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+            var copyData = (T) Marshal.PtrToStructure(pointer, typeof(T));
+            Marshal.FreeHGlobal(pointer);
+
+            return copyData;
+        }
+
+
+        public static byte[] ToArray(this object data)
+        {
+            var size = Marshal.SizeOf(data);
+            byte[] buffer = new byte[size];
+            var pointer = Marshal.AllocHGlobal(size);
+            Marshal.StructureToPtr(data, pointer, true);
+
+            int startIndex = 0;
+            Marshal.Copy(pointer, buffer, startIndex, size);
+            Marshal.FreeHGlobal(pointer);
+
+            return buffer;
+        }
     }
 }
